adc211f67c482da866d5e754f8cc01ca024fdee3
Jhard <j.mideh2@gmail.com>
2022-12-08 19:25:45 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
+
 /**
+ * main - Entry poin
  *
- * main - Entry point
- *
- *   
- *
- * Return: Always 0 (Success)
- *
+ * Return: Always O (Success)
  */
 int main(void)
 {
 	puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+	return (0)
 }
